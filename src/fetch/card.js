e90f31a5be2f5d23677790237cb4b5b77721a37f
sumanth <sumanthchinna047@gmail.com>
2024-06-21 19:04:36 +0530

    second change

--- src/fetch/card.js
+++ src/fetch/card.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './card.css';
 
-const Card = ({ flag, name, population, capital, region }) => {
+const Card = ({ flag, name, population, capital, region, coatOfArms }) => {
     return (
+        <div className='card-containers'>
         <div className="card">
-            <div className="flag">{flag}</div>
+            <img width={150} height={100} className="flag" src={flag?.svg} alt={flag?.alt}/>
             <h3>{name}</h3>
             <p>Population: {population}</p>
             <p>Capital: {capital}</p>
             <p>Region: {region}</p>
+        </div>
+        <img style={{padding:'10px'}} width={50} height={50} className="coatOfArms" src={coatOfArms?.svg} alt={coatOfArms?.alt}/>
         </div>
     );
 }
--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -7,6 +7,7 @@ const Change = () => {
     const [data, setData] = useState([])
     const [filterRegion, setFilterRegion] = useState('')
     const [regions, setRegions] = useState([])
+    const [country, setCountry] = useState('')
 
     useEffect(() => {
         axios.get("https://restcountries.com/v3.1/independent?status=true").then(
@@ -18,19 +19,29 @@ const Change = () => {
         )
     }, [])
 
-    const handleFilterChange = (event) => {
-        setFilterRegion(event.target.value)
-    };
-
-    const filteredData = filterRegion 
-        ? data.filter(item => item.region.toLowerCase() === filterRegion.toLowerCase())
-        : data
+    const filteredData = data.filter(item => {
+        const regionMatch = filterRegion ? item.region.toLowerCase() === filterRegion.toLowerCase() : true;
+        const countryMatch = country ? item.name.common.toLowerCase().includes(country.toLowerCase()) : true;
+        return regionMatch && countryMatch;
+    })
 
     return (
         <div>
+            <center>
+                <div className='search'>
+                    <input 
+                        onChange={(event) => setCountry(event.target.value)} 
+                        type='text' 
+                        value={country} 
+                        placeholder='Search for a country...'
+                    />
+                    <h3>{country}</h3>
+                </div>
+            </center>
+
             <select
                 value={filterRegion}
-                onChange={handleFilterChange}
+                onChange={(event) => setFilterRegion(event.target.value)}
                 className="filter-select"
             >
                 <option value="">All Regions</option>
@@ -42,11 +53,12 @@ const Change = () => {
                 {filteredData.map(item => (
                     <Card
                         key={item.cca3}
-                        flag={item.flag}
-                        name={item.name.official}
+                        flag={item.flags}
+                        name={item.name.common}
                         population={item.population}
                         capital={item.capital}
                         region={item.region}
+                        coatOfArms={item.coatOfArms}
                     />
                 ))}
             </div>
